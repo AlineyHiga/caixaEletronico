2d8f5b59f6206d09588df7b20d946ba68130ebcd
AlineyHiga <alineyhiga@gmail.com>
2023-07-25 21:03:09 -0300

    chore: fix accessibility voice over

--- app/src/app/shared/components/dialog-nota/dialog-nota.component.ts
+++ app/src/app/shared/components/dialog-nota/dialog-nota.component.ts
@@ -1,13 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dialog-nota',
   templateUrl: './dialog-nota.component.html',
   styleUrls: ['./dialog-nota.component.css'],
   standalone:true,
-  imports: [MatDialogModule, NgFor, NgIf],
+  imports: [MatDialogModule, NgFor, NgIf,MatButtonModule, MatIconModule],
 })
 export class DialogNotaComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any){}
